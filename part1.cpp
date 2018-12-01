7850a9db2bc50b1bd6cf365b386aac0cfc30c4c6
Alberto Rocha <berto323@csu.fullerton.edu>
2018-11-30 20:58:12 -0800

    Made Changes to part1.cpp
    Created the test file finalp1.txt
    Created outout file output.txt
    Made some changeds to the while loop instead of reading by line it is reading by char.
    there is a bug when it read the last word. The text that is in the output is currently what the code outputs

--- part1.cpp
+++ part1.cpp
@@ -6,22 +6,80 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	string filename;
-	string line;
+	string line ;
+
+	char current;
+	char meme[2];
+	int tracker = 0;
 	ifstream input;
-	
-	cout << "Enter filename: ";
-	cin >> filename;
-	input.open(filename);
+	ofstream output;
+
+
+	//cout << "Enter filename: ";
+	//cin >> filename;
+	input.open("/Users/albertorocha/Documents/GitHub/Final_323/finalp1.txt");
+	output.open("/Users/albertorocha/Documents/GitHub/Final_323/output.txt");
 
 	if(!input.is_open()){
-		cout << "ERROR....Unable to open file.";
+		cout << "ERROR....Unable to open input file.";
+		exit(0);
+	}
+	if(!output.is_open()){
+		cout << "ERROR....Unable to open output file.";
 		exit(0);
 	}
 
-	while (getline(input, line) && !input.eof()) {
-		cout << line << endl;
+	while (!input.eof() ) {
+
+		input.get(current);
+
+
+			if(current == '/' || current == '*'){
+
+				if (tracker >= 2){
+					tracker = 0;
+				}
+				meme[tracker] = current;
+
+				tracker ++;
+
+			}
+			else if( meme[0] == '/' && meme[1]== '/'){
+
+				if(current == '\n') {
+					meme[0]= ' ';
+					meme[1] = ' ';
+
+				}
+			}
+			else if( meme[0] == '/' && meme[1]== '*'){
+
+
+
+			}
+			else if (meme[0] == '*' && meme[1]== '/'){
+				meme[0]=' ';
+				meme[1] = ' ';
+			}
+
+			else if(current !=' ') {
+
+				line += current;
+			}
+
+
+			if(current == '\n' || current == '\0') {
+
+				output << line;
+				line = "";
+			}
+
 	}
 
+output << '\n' << line;
+
+
 	input.close();
+	output.close();
 	return 0;
 }
